fd8190934a515f23899334250e351f6293ff6b0b
lalankeba <lalankeba@gmail.com>
2024-01-20 23:39:54 +0200

    adding max upload file size

--- src/main/java/com/laan/wordfinder/controller/exception/ExceptionController.java
+++ src/main/java/com/laan/wordfinder/controller/exception/ExceptionController.java
@@ -2,16 +2,21 @@ package com.laan.wordfinder.controller.exception;
 
 import com.laan.wordfinder.exception.WordFrequencyException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionController {
+
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
 
     @ExceptionHandler(WordFrequencyException.class)
     public ProblemDetail onWordFinderException(WordFrequencyException exception) {
@@ -29,6 +34,12 @@ public class ExceptionController {
     public ProblemDetail onMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
         log.error("MethodArgumentTypeMismatchException occurred. {}", exception.getMessage());
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ProblemDetail onMaxUploadSizeExceededException(MaxUploadSizeExceededException exception) {
+        log.error("MaxUploadSizeExceededException occurred. {}", exception.getMessage());
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage() + ". Max file size: " + maxFileSize);
     }
 
     @ExceptionHandler(Exception.class)
